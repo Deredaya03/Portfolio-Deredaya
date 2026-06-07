155da241959c142045855323772f95d81a966aa4
Deredaya03 <capederedaya@gmail.com>
2026-06-06 21:14:50 -0600

    Iniciar Portfolio

---
+++ src/scripts/intro.js
@@ -0,0 +1,29 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+},{
+  threshold: 0.1
+});
+
+const elements = document.querySelectorAll('.scroll-fade');
+elements.forEach((el) => observer.observe(el));
+
+const boton = document.getElementById('btn-copiar');
+
+boton.addEventListener('click', () => {
+  const email = "deredaya.adm@gmail.com";
+
+  navigator.clipboard.writeText(email)
+    .then(() => {
+      boton.textContent = "¡Copiado!";
+      setTimeout(() => boton.textContent = "Copiar enlace", 2000);
+    })
+    .catch(err => {
+      console.error('Error al copiar al portapapeles: ', err);
+    });
+});
